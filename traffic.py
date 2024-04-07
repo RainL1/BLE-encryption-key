a04860305a84060741e7cfbe15fe8a14aaf95d46
RainL1 <153647135+RainL1@users.noreply.github.com>
2024-04-07 21:10:41 +0300

    Delete traffic.py

--- traffic.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-process = subprocess.Popen('tshark -i bluetooth0 -a duration:20 -w pack.pcap', shell=True)
-a = open('conn.txt', 'r+')
-a.truncate(0)
-a.write(str(process.pid))
-a.close()
